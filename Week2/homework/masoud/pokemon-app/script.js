39e4a5fa48cce11b3e38a6ae11eb7b32fb68e866
mas <mas.sam1368@gmail.com>
2020-12-22 23:33:54 +0330

    Fixed Ex3

--- Week2/homework/masoud/pokemon-app/script.js
+++ Week2/homework/masoud/pokemon-app/script.js
@@ -2,63 +2,68 @@
 window.onload = main;
 
 //Define main function
-function main(data) {
-  // Define input element
-  const input = document.createElement('input');
-  input.type = 'number';
-  input.min = '1';
-  input.max = '895';
-  input.style.width = '110px';
-  input.placeholder = 'Insert a number';
-  document.body.appendChild(input);
-
+function main() {
   //Define button element
   const btn = document.createElement('button');
   btn.textContent = 'start';
   btn.style.display = 'block';
   document.body.appendChild(btn);
 
+  // Define select element
+  const select = document.createElement('select');
+  document.body.appendChild(select);
+
   //When button is clicked
   btn.addEventListener('click', () => {
-    if (input.value > 0 && input.value < 896) { //Check input
-      const url = `https://pokeapi.co/api/v2/pokemon/${input.value}`;//Define API URL
-      fetchData(url); // Get data from API and insert to DOM
-    } else {
-      alert('Your input number must be between 0 and 895!')
-    };
+    const url = `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20`;//Define API URL
+    fetchData(url, select); // Get data from API and insert to DOM
   });
-};
+}
 
 // Add data to DOM
-function addPokemonToDOM(data) {
-  // Define img element
-  const imgSRC = data.sprites.back_default
-  const image = document.createElement('img');
-  image.src = imgSRC;
-  document.body.appendChild(image);
-  //disapear img element after 1.5 second
-  window.setTimeout(() => {
-    document.body.removeChild(image);
-  }, 1500);
+function addPokemonToDOM(data, select) {
+
+  //Add list to select tag
+  data.results.forEach(element => {
+    const option = document.createElement('option');
+    option.value = element.name;
+    option.textContent = element.name;
+    select.appendChild(option);
+  });
+
+  //User selection 
+  select.addEventListener('input', () => {
+
+    data.results.forEach(element => {
+      if (select.value == element.name) {
+        const imgURL = element.url;
+        fetch(imgURL) // second API request to get image
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (myJson) {
+            const img = document.createElement('img');
+            img.src = myJson.sprites.back_default;
+            document.body.appendChild(img);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+    })
+  })
 };
 
 //Get data from API by using fetch API
-function fetchData(url) {
+function fetchData(url, select) {
   fetch(url) // First getting data
     .then(function (response) {
       return response.json();
     })
     .then(function (myJson) {
-      addPokemonToDOM(myJson); // Add data to DOM
-      return fetch(url); //Second getting data
-    })
-    .then((response => {
-      return response.json();
-    }))
-    .then((myJson) => {
-      addPokemonToDOM(myJson);// Add data to DOM
+      addPokemonToDOM(myJson, select); // Add data to DOM
     })
     .catch(function (error) {
-      console.log('error:' + error);
+      console.log(error);
     });
 };
