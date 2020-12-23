7fdf479c5e75935b5c408bd02eb05dc445323f96
Amirhossein <ahshadravan@gmail.com>
2020-12-24 00:09:05 +0330

    Element factory function is added

--- hackyourrepo-app/AmirHossein/script.js
+++ hackyourrepo-app/AmirHossein/script.js
@@ -25,9 +25,26 @@ const container = elFactory('div', {calss: 'container'},
   elFactory('section', {id: 'header'},
     elFactory('p', {}, 'HYF Repositories'),
     elFactory('select', {calss: 'selectMenu'},
-    elFactory('option', {value: ''}, 'alumni'))
-  ));
+    elFactory('option', {value: ''}, 'alumni'))),
+  elFactory('div', {class: 'bottom-box'},
+    elFactory('section', {id: 'left-side'},
+      elFactory('div', {calss: 'card'},
+        elFactory('table', {},
+          elFactory('tr', {},
+            elFactory('td', {class: 'col-left'}, 'Repositories: '),
+            elFactory('td', {class: 'col-right rep-text'}, elFactory('a', {href: '#', class: 'repo-link'}, 'SampleRepo1')) ),
+          elFactory('tr', {},
+            elFactory('td', {class: 'col-left'}, 'Description: '),
+            elFactory('td', {class: 'col-right rep-description'}, 'Lorem ipsom')),
+          elFactory('tr', {},
+            elFactory('td', {class: 'col-left'}, 'Forks: '),
+            elFactory('td', {class: 'col-right rep-fork'}, '5')),
+          elFactory('tr', {},
+            elFactory('td', {class: 'col-left'}, 'Update: '),
+            elFactory('td', {class: 'col-right rep-update'}, '2020-05-27 12:00:00'))))))
+    );
 
+  console.log(container);
 const select = container.querySelector('select')
 const para = container.querySelector('p')
 console.log(para)
