4a5c10a92f241dd1717701382955f90679fc45bd
Amirhossein <ahshadravan@gmail.com>
2020-12-23 19:12:27 +0330

    Week2 project started

--- hackyourrepo-app/AmirHossein/script.js
+++ hackyourrepo-app/AmirHossein/script.js
@@ -3,24 +3,53 @@
 /*
   Write here your JavaScript for HackYourRepo!
 */
-const placeholderRepos = [
-  {
-    name: 'SampleRepo1',
-    description: 'This repository is meant to be a sample',
-    forks: 5,
-    updated: '2020-05-27 12:00:00',
-  },
-  {
-    name: 'AndAnotherOne',
-    description: 'Another sample repo! Can you believe it?',
-    forks: 9,
-    updated: '2020-05-27 12:00:00',
-  },
-  {
-    name: 'HYF-Is-The-Best',
-    description:
-      "This repository contains all things HackYourFuture. That's because HYF is amazing!!!!",
-    forks: 130,
-    updated: '2020-05-27 12:00:00',
-  },
-];
+const elFactory = (type, attributes, ...children) => {
+  const el = document.createElement(type)
+  let key;
+  for (key in attributes) {
+    el.setAttribute(key, attributes[key])
+  }
+
+  children.forEach(child => {
+    if (typeof child === 'string') {
+      el.appendChild(document.createTextNode(child))
+    } else {
+      el.appendChild(child)
+    }
+  })
+
+  return el
+}
+
+const container = elFactory('div', {calss: 'container'},
+  elFactory('section', {id: 'header'},
+    elFactory('p', {}, 'HYF Repositories'),
+    elFactory('select', {calss: 'selectMenu'},
+    elFactory('option', {value: ''}, 'alumni'))
+  ));
+
+const select = container.querySelector('select')
+const para = container.querySelector('p')
+console.log(para)
+
+
+const header = container.querySelector('p')
+
+
+document.body.appendChild(container)
+
+
+
+
+
+
+
+
+
+
+// const container = document.createElement('div');
+// container.classList.add('container');
+// const header = document.createElement('section');
+// const title = document.createElement('')
+// const selectMenu = document.createElement('select');
+
